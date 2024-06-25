6b8bed791442169fede27fc07649c7bb31dcd987
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-06-25 13:38:52 +0530

    navabr and menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,32 @@
+import Link from "next/link";
+import Menu from "./Menu";
+import Image from "next/image";
+
 const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <div className="h-20 px-4 md:px-9 lg:px-16 xl:px-32 2xl:px-64 relative">
+      {/* Mobile */}
+      <div className=" h-full flex items-center justify-between">
+        <Link href="/">
+          <div className="text-2xl tracking-wide">FableFinds</div>
+        </Link>
+        <Menu />
+      </div>
+
+      {/* Larger screen */}
+      <div className="hidden md:flex items-center h-full justify-between gap-8">
+        {/* left */}
+        <div className="w-1/3">
+          <Link href="/">
+          <Image src="/logo.png" alt="" width={24} height={24}/>
+            <div className="text-2xl tracking-wide">FableFinds</div>
+          </Link>
+        </div>
+        {/* right */}
+        <div className="w-2/3"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
